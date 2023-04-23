be9ca57a2cd105b2a1174309ce5e77966fcb7b89
Ujjwal <ujjwalbhardwaj5809@gmail.com>
2023-04-23 21:28:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React, { useState, useEffect, memo } from "react"; import PropTypes from "prop-types"; 
+// Single List Item 
+const WrappedSingleListItem = ({ index, isSelected,  onClickHandler, text }) => { 
+const handleClick = () => { 
+onClickHandler(index); 
+};
+return ( 
+<li 
+style={{ backgroundColor: isSelected ? "green" : "red" }} 
+onClick={handleClick} 
+> 
+{text} 
+</li> 
+); 
+}; 
+WrappedSingleListItem.propTypes = { 
+index: PropTypes.number, 
+isSelected: PropTypes.bool, 
+onClickHandler: PropTypes.func.isRequired, 
+text: PropTypes.string.isRequired 
+}; 
+const SingleListItem = memo(WrappedSingleListItem); 
+// List Component 
+const WrappedListComponent = ({ items }) => { 
+const [selectedIndex, setSelectedIndex] = useState(); 
+useEffect(() => { 
+setSelectedIndex(null); 
+}, [items]); 
+const handleClick = (index) => { 
+selectedIndex === index ? setSelectedIndex(null) :  setSelectedIndex(index); 
+}; 
+// console.log(selectedIndex); 
+return ( 
+<ul style={{ textAlign: "left" }}> 
+{items.map((item, index) => ( 
+<SingleListItem 
+key={index} 
+onClickHandler={() => handleClick(index)}
+text={item.text} 
+index={index} 
+isSelected={selectedIndex === index} /> 
+))} 
+</ul> 
+); 
+}; 
+WrappedListComponent.propTypes = { 
+items: PropTypes.arrayOf( 
+PropTypes.shape({ 
+text: PropTypes.string.isRequired 
+}) 
+) 
+}; 
+WrappedListComponent.defaultProps = { items: [ 
+ { text: "Task1" }, 
+{ text: "Task2" }, 
+{ text: "Task3" }, 
+{ text: "Task4" } 
+] 
+}; 
+const List = memo(WrappedListComponent); 
+export default List;
